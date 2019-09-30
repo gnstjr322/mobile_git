d9544e14eda0f3d66f03383e1e01c71f2e202402
koreaaerospaceU <zx811@naver.com>
2019-09-30 18:23:10 +0900

    server 1.4

--- Server/Server_proto.js
+++ Server/Server_proto.js
@@ -4,6 +4,8 @@ var http = require('http');
 var nic_ip = '192.168.1.71';
 var bodyParser= require('body-parser');
 var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
 /*
 let users = [
   {
@@ -13,12 +15,41 @@ let users = [
   }
 ]
 */
+/*
 app.get('/', (req, res) => {
   console.log("Start SCrapping");
-  //var id = req.param('id');//안드로이드 접속시 파리미터 id를 받는다
-  //var pw = req.param('pw');//안드로이드 접속시 파리미터 pw를 받는다
+  var id1 = req.query.id;//안드로이드 접속시 파리미터 id를 받는다
+  var pw1 = req.query.pw;//안드로이드 접속시 파리미터 pw를 받는다
+  console.log(id1);
+  console.log(pw1);
   var id = "2015125054"
   var pw = "jooboo100@"
+  var result = crawler.crawler(id,pw,function(result){
+    res.json(result);
+  })
+});
+*/
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended : false }));
+
+app.post('/', (req, res) => {
+  console.log("Start SCrapping");
+  //var id1 = req.body.id;//안드로이드 접속시 파리미터 id를 받는다
+  //var pw1 = req.body.pw;//안드로이드 접속시 파리미터 pw를 받는다
+  //var aa = req.body;//안드로이드 접속시 파리미터 id를 받는다
+  //var pw = req.param();//안드로이드 접속시 파리미터 pw를 받는다
+  var id = req.param("id");
+	var pw = req.param("pw");
+	//res.writeHead("200", {"Content-Type":"text/html;charset=utf8"});
+	//res.write("user uId : " + uId);
+	//res.write("<br>");
+	//res.write("userd uPw : " + uPw);
+	//res.write("<a href='/login_post.html'>login</a>");
+	//res.end();
+  /////
+  console.log("id: "+id+"pw: "+ pw);
+  //var id = "2015125054"
+  //var pw = "jooboo100@"
   var result = crawler.crawler(id,pw,function(result){
     res.json(result);
   })
